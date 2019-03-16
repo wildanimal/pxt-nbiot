3a37147af0762ddfb5cb3f3a8252af165dd86bac
我 <8514978@qq.com>
2019-03-17 04:02:59 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,19 +1,30 @@
+nbiot.onReceivedString(function (text) {
+    basic.showString(nbiot.readLine())
+    basic.showIcon(IconNames.SmallSquare)
+})
 nbiot.onConnected(function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Yes)
+    nbiot.sendString("Hello World!")
 })
-
 input.onButtonPressed(Button.A, function () {
-    nbiot.sendNumber(123)
+    //basic.showString(nbiot.imei())
+    // basic.showString(nbiot.imei()) nbiot.writeCommand(
+    // "AT+CGSN=1", 0, 0, 0 )
+    // basic.showString(nbiot.readLine())
+    nbiot.sendString("gps  path:19/0/0, payload:40240000000000004024000000000000")
+    basic.showIcon(IconNames.SmallDiamond)
 })
-
 input.onButtonPressed(Button.B, function () {
-    nbiot.sendString("Hello")
+    // basic.showString(nbiot.imsi())
+    nbiot.sendString("nurse  path:19/0/0, payload:00000169873b3fe800000169873b43d0")
+    basic.showIcon(IconNames.Tortoise)
 })
-
+basic.showIcon(IconNames.SmallHeart)
+nbiot.connect(SerialPin.P8, SerialPin.P12)
+basic.pause(1000)
+// nbiot.setServer("221.229.214.202", 5683)
+// basic.pause(1000)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    nbiot.sendString("" + input.temperature())
-    basic.pause(60000)
-})
 
-basic.showIcon(IconNames.Sad)
-nbiot.connect(SerialPin.P0, SerialPin.P1)
+})
